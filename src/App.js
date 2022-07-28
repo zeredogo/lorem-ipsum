93c04d68be34abe75193f08f197b1dc4796246a6
zeredogo <zeredogo@gmail.com>
2022-07-28 23:35:58 +0100

    update made

--- src/App.js
+++ src/App.js
@@ -13,9 +13,18 @@ function App() {
   }
   
   return (
-  <section className='sction-center'>
+  <section className='section-center'>
     <h3>tired of boring lorem ipsum?</h3>
-    <form className='lorem-form' onSubmit={handleSubmit}></form>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor='amount'>
+        paragraphs:
+      </label>
+      <input type='number' name='amount' id='amount' 
+      value={count}
+      onChange={(e) => setCount(e.target.value) }
+      />
+      <button type='submit' className='btn'>generate</button>
+    </form>
   </section>
     )
 }
