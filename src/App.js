290cee30c03815727b5996aff300ac3b238cc241
zeredogo <zeredogo@gmail.com>
2022-07-28 23:19:58 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import './App.css'
+import data from './data';
+function App() {
+  
+  
+  return (
+  <h2>lorem ipsum project setup</h2>
+    )
+}
+
+export default App;
